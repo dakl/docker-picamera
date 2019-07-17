ce58131a11a3eb37c927a7ef53ddd14c7bdec622
Daniel Klevebring <daniel.klevebring@gmail.com>
2019-07-17 18:14:46 +0000

    use dakl/picamera

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 docker run -d --restart=always \
+  --name=picamera \
   --device /dev/vchiq \
   -p 8000:8000 \
   -e AUTH_USERNAME=pi \
   -e AUTH_PASSWORD=picamera \
-  pschmitt/picamera
+  -e VFLIP=true \
+  dakl/picamera
